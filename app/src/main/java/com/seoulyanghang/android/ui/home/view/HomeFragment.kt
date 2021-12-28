986d2a7fa2f8e0bc9671f24f5bd22859c8425931
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2021-12-28 19:50:48 +0900

    [FEAT] Viewmodel초기화, 화면 클릭시 키보드 내리기, InitView()로 view초기화

--- app/src/main/java/com/seoulyanghang/android/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/seoulyanghang/android/ui/home/view/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.seoulyanghang.android.ui.home.view
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,24 +9,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.NonNull
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.seoulyanghang.android.R
 import com.seoulyanghang.android.base.BaseViewUtil
 import com.seoulyanghang.android.databinding.FragmentHomeBinding
 import com.seoulyanghang.android.databinding.FragmentItemInquiryBinding
+import com.seoulyanghang.android.ui.home.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.MotionEvent
+
+import androidx.core.content.ContextCompat.getSystemService
+
 
 @AndroidEntryPoint
 class HomeFragment : BaseViewUtil.BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val homeViewModel: HomeViewModel by viewModels()
     private val handler: MyHandler by lazy { MyHandler() }
     private var currentPosition = Int.MAX_VALUE / 2
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.homeViewModel = homeViewModel
+        binding.lifecycleOwner = this
+        initView()
+    }
 
+    private fun initView() {
+        initRootClickEvent()
         setViewPagerAdapter(requireActivity())
-        setViewPagerCount()
+    }
+
+    //화면 전체 아무곳이나 눌러도 키보드 내려가도록
+    private fun initRootClickEvent() {
+        binding.clRoot.setOnClickListener {
+            ViewCompat.getWindowInsetsController(it)?.hide(WindowInsetsCompat.Type.ime())
+        }
     }
 
     //ViewPager연결
@@ -34,28 +61,38 @@ class HomeFragment : BaseViewUtil.BaseFragment<FragmentHomeBinding>(R.layout.fra
         binding.vpHome.apply {
             adapter = viewPagerAdapter
             setCurrentItem(currentPosition, false)
+
+            // ViewPager2의 현재 위치 표시+좌우 무한 스크롤
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                //선택된 ViewPager의 position을 알려줌
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.tvHomeCurrentPage.text = "${position % 3 + 1}"
+                }
+
+                //스크롤 상태에 따라 자동 스크롤 처리
+                override fun onPageScrollStateChanged(state: Int) {
+                    super.onPageScrollStateChanged(state)
+                    when (state) {
+                        ViewPager2.SCROLL_STATE_IDLE -> autoScrollStart(INTERVAL_TIME)
+                        ViewPager2.SCROLL_STATE_DRAGGING -> autoScrollStop()
+                    }
+                }
+            })
         }
     }
 
-    //ViewPager2의 현재 위치 표시+좌우 무한 스크롤
-    private fun setViewPagerCount() {
-        binding.vpHome.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            //선택된 ViewPager의 position을 알려줌
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                binding.tvHomeCurrentPage.text = "${position % 3 + 1}"
+    private fun setUIListener() {
+        with(binding) {
+            //상품검색 버튼 클릭
+            ivHomeSearch.setOnClickListener {
             }
 
-            //스크롤 상태에 따라 자동 스크롤 처리
-            override fun onPageScrollStateChanged(state: Int) {
-                super.onPageScrollStateChanged(state)
-                when (state) {
-                    ViewPager2.SCROLL_STATE_IDLE -> autoScrollStart(INTERVAL_TIME)
-                    ViewPager2.SCROLL_STATE_DRAGGING -> autoScrollStop()
-                }
-            }
-        })
-    }
+
+            //리사이클러뷰 아이템 클릭
+        }
+   }
+
 
     //ViewPager 자동 스크롤을 위한 Handler
     private inner class MyHandler : Handler(Looper.getMainLooper()) {
