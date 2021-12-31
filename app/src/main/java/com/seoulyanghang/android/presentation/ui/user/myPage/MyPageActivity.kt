bd289b066435c99617b33c24e7184d7eac4ca1ba
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2021-12-31 13:06:09 +0900

    [ADD] Mypage Activity, UserAccount생성

---
+++ app/src/main/java/com/seoulyanghang/android/presentation/ui/user/myPage/MyPageActivity.kt
@@ -0,0 +1,13 @@
+package com.seoulyanghang.android.presentation.ui.user.myPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seoulyanghang.android.R
+import com.seoulyanghang.android.databinding.ActivityMyPageBinding
+import com.seoulyanghang.android.presentation.base.BaseViewUtil
+
+class MyPageActivity : BaseViewUtil.BaseAppCompatActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
